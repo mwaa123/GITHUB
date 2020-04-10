85a99b1029bd125672903f257e38f01209ac05a1
ruth <ruthwanjiramugo@gmail.com>
2020-04-10 07:12:39 +0300

    add router for the about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutComponent } from './about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+ { path:'', redirectTo:'/landing-page', pathMatch:'full'},
+{ path: 'landing-page', component: LandingPageComponent},
+{ path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponets =[ LandingPageComponent,AboutComponent ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { AppRoutingModule,routingComponets } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HubComponent } from './hub/hub.component';
+// import { LandingPageComponent } from './landing-page/landing-page.component';
+// import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HubComponent,
+    routingComponets
+    // LandingPageComponent,
+    // AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
